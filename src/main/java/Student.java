b9224fb5e88ba73ec0cb156db4471c066b7b5596
Rajib Chaudhary <rajibchaudhary2020@gmail.com>
2022-12-05 15:50:05 -0500

    Student Java created

---
+++ src/main/java/Student.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Student {
+    public long id;
+    public String name;
+    ArrayList<Integer>grades;
+
+    public Student(long id, String name){
+        this.id = id;
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    // returns the student's id
+    public long getId(){
+        return this.id;
+    }
+
+    // returns the student's name
+    public String getName(){
+        return this.name;
+    }
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    // returns the list of grades
+    public ArrayList<Integer> getGrades(){
+        return this.grades;
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        double sum = 0;
+        for(int grade: grades){
+            sum += grade;
+        }
+        return sum/grades.size();
+    }
+
+}
